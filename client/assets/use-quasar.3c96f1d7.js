fff57e2dca0862e62d8305ad136967fdde57ce44
dvmaximov <dvmaximov@mail.ru>
2023-10-30 16:27:56 +0300

    Update

---
+++ client/assets/use-quasar.3c96f1d7.js
@@ -0,0 +1,1 @@
+import{i as a,af as r}from"./index.1a710307.js";function u(){return a(r)}export{u};
--- client/assets/use-quasar.40b0fec9.js
+++
@@ -1,1 +0,0 @@
-import{i as a,af as r}from"./index.4bc2e000.js";function u(){return a(r)}export{u};
