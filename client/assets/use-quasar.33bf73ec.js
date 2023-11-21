23184613910a2cf278c1f6e579db03a2e3c3cca6
dvmaximov <dvmaximov@mail.ru>
2023-11-22 01:02:59 +0300

    Update

--- client/assets/use-quasar.33bf73ec.js
+++
@@ -1,1 +0,0 @@
-import{i as a,af as r}from"./index.c32f5f97.js";function u(){return a(r)}export{u};
---
+++ client/assets/use-quasar.a4b8c930.js
@@ -0,0 +1,1 @@
+import{i as a,af as r}from"./index.94431755.js";function u(){return a(r)}export{u};
