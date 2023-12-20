df9016dacb6ff09e2022fb8116f37b61b7732088
dvmaximov <dvmaximov@mail.ru>
2023-12-20 21:54:04 +0300

    Update

--- client/assets/use-quasar.efd27bb6.js
+++
@@ -1,1 +0,0 @@
-import{i as a,af as r}from"./index.3ae2caef.js";function u(){return a(r)}export{u};
---
+++ client/assets/use-quasar.ff10fabc.js
@@ -0,0 +1,1 @@
+import{i as a,af as r}from"./index.4441ceb3.js";function u(){return a(r)}export{u};
